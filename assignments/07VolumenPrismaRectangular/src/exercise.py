b6eda0c242f8b5a83584a37871b45360487c88b9
Frida Velez <a00835434@tec.mx>
2021-09-08 13:11:47 -0500

    inicial

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -2,7 +2,7 @@ b = float(input("Dame la base:"))
 a = float(input("Dame la altura: "))
 p = float(input("Dame la profundidad: "))
     
-volumen_prisma= b*p*a
+volumen_prisma=b*p*a
     
 r = volumen_prisma
 
