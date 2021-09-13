a7e9e80448cc0db779f3d6bf709aaa3f209d8ec6
Frida Velez <a00835434@tec.mx>
2021-09-12 21:19:35 -0500

    correccionn

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,9 +1,15 @@
-b = float(input('Dame la base: '))
-a = float(input("Dame la altura:"))
-p = float(input("Dame la profundidad:"))
-    
-volumen_prisma=b*p*a
-    
-r = volumen_prisma
+def main():
 
-print("El volumen del prisma es:", r)
+  b = float(input('Dame la base: '))
+  a = float(input("Dame la altura:"))
+  p = float(input("Dame la profundidad:"))
+    
+  volumen_prisma=b*p*a
+    
+    
+  r = volumen_prisma
+
+  print("El volumen del prisma es:", r)
+
+if __name__ == '__main__':
+    main()
